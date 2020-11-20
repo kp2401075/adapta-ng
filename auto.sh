5f995e5495955a50a875e6eab2dc5172f3188344
Jay Patel <kp2401075@gmail.com>
2020-11-21 01:25:34 +1100

    initial-commit

---
+++ auto.sh
@@ -0,0 +1,11 @@
+./autogen.sh --prefix=/home/jayu --with-selection_color=#00BCD4 --with-accent_color=#4DB6AC --with-suggestion_color=#009688 \
+--with-accent_color=#4DB6AC \
+--with-suggestion_color=#009688 \
+--with-destruction_color=#FF5252 \
+--disable-xfce \
+--disable-mate \
+--disable-openbox
+
+make
+
+make install
